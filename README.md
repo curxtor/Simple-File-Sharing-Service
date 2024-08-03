d7e899dc681c761cfbdc3e04c82e348250d71894
ro_sv <ro.sv404@gmail.com>
2024-08-03 17:17:46 +0700

    alpha commit

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-# Simple File Sharing Service with Vue3 [![Vue3](https://simpleicons.org/icons/vuedotjs.svg)]
-=======
-# tasky
+# Simple File Sharing Service with Vue3
+<svg role="img" viewBox="0 0 24 24" width="100" xmlns="http://www.w3.org/2000/svg"><title>Vue.js</title><path d="M24,1.61H14.06L12,5.16,9.94,1.61H0L12,22.39ZM12,14.08,5.16,2.23H9.59L12,6.41l2.41-4.18h4.43Z"/></svg>
 
 ## Project setup
 ```
@@ -16,12 +15,3 @@ npm run serve
 ```
 npm run build
 ```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
->>>>>>> 317ca38 (init)
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,14 +15,14 @@
       @click="inputClick"
       v-else
     >
-      <p style='font-size: 22px;'>Select or drop file</p>
+      <p>Select or drop file</p>
     </div>
     
     <div 
       class='submitButton'
       @click='sendButton'
     >
-      <p>Получить ссылку</p>
+      <p>Get URL</p>
     </div>
     <input
         type="file"
@@ -34,6 +34,10 @@
         hidden
         multiple
       />
+      <div class='copyLink' @click='copyLink' v-if='this.downloadLink != ""'>
+        {{this.downloadLink}}
+        <svg style='width: 1vw;' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M208 0H332.1c12.7 0 24.9 5.1 33.9 14.1l67.9 67.9c9 9 14.1 21.2 14.1 33.9V336c0 26.5-21.5 48-48 48H208c-26.5 0-48-21.5-48-48V48c0-26.5 21.5-48 48-48zM48 128h80v64H64V448H256V416h64v48c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V176c0-26.5 21.5-48 48-48z"/></svg>
+      </div>
       
       
     
@@ -50,39 +54,39 @@ export default {
       uploaded: false,
       files: [],
       progressUploading: 0,
-      loading: false
+      loading: false,
+      downloadLink: ""
     }
   },
   methods: {
     sendButton() {
       if (this.files.length != 0) {
+        this.loading = true
         const fileInput = document.getElementById('fileInput');
         const files = fileInput.files;
         const formData = new FormData();
         for (let i = 0; i < files.length; i++) {
             formData.append('files[]', files[i]);
         }
-        this.loading = true
-        axios.post('http://localhost:5000/sharing', formData, {
+        
+        axios.post('http://'+ location.host +':5000/sharing', formData, {
           onUploadProgress: (progressEvent) => {
-            
-            const totalLength = progressEvent.lengthComputable ? progressEvent.total : progressEvent.target.getResponseHeader('content-length') || progressEvent.target.getResponseHeader('x-decompressed-content-length');
             let progress = document.querySelector('.progressLoad')
+            const { loaded, total } = progressEvent;
+            let percent = Math.floor((loaded * 100) / total);
+            this.progressUploading = percent
+            if (this.loading == true) {
+              progress.style.background = `linear-gradient(to right, #84ffc8 ${this.progressUploading}%, white 0%)`
+            }
             
-            if (totalLength !== null) {
-                this.progressUploading = Math.round((progressEvent.loaded * 100) / totalLength)
-            }
-            console.log(this.progressUploading)
-            progress.style.background = `linear-gradient(to right, #84ffc8 ${this.progressUploading}%, white 0%)`
-            if (this.progressUploading >= 80) {
-              this.progressUploading = 100
-              progress.style.background = `linear-gradient(to right, #84ffc8 100%, white 0%)`
-              this.loading = false
-            }
+            
           }
         })
           .then(res => {
-            console.log(res)
+            this.downloadLink = res.data.message
+            let progress = document.querySelector('.progressLoad')
+            progress.style.background = `linear-gradient(to right, #84ffc8 100%, white 0%)`
+            this.loading = false
             
           })
           .catch(error => {
@@ -104,13 +108,20 @@ export default {
       else {
         this.files = this.$refs.file.files;
         let inputText = document.querySelector('.input_box p')
-        inputText.innerHTML = "Файл успешно загружен"
+        inputText.innerHTML = "File is selected"
         toast.success(`You add a new file with name: ` + this.files[0].name, {
           timeout:4000
         })
-      }
-      
-      
+      } 
+    },
+    copyLink() {
+      let toast = useToast()
+      document.body.insertAdjacentHTML('beforeend', `<input id="temp-copy" value = ${this.downloadLink}>`);
+      let copyText = document.getElementById('temp-copy');
+      copyText.select();
+      document.execCommand("copy");
+      copyText.remove();
+      toast.success("URL successfully copied!", {timeout: 2000})
     },
     dragover(e) {
       e.preventDefault();
@@ -148,6 +159,7 @@ export default {
   margin: 0 auto;
   transition: 0.3s;
   cursor:pointer;
+  margin-bottom: 5px;
 }
 .submitButton:hover {
   background-color: #42b983;
@@ -197,5 +209,44 @@ a {
   border-style: solid;
   transition: 0.1s;
   height: 8vh;
+}
+.input_box p {
+    font-size: 2.5vh;
+  }
+.copyLink {
+  border: 2px solid #000000;
+  display: inline-block;
+  padding: 6px 7px 6px 7px;
+  border-radius:5px;
+  transition: 0.3s;
+  cursor: pointer;
+}
+.copyLink:hover {
+  background-color: #000000;
+  color: white;
+  transition: 0.3s;
+  svg {
+    fill:white;
+  }
+}
+@media (max-width: 768px) {
+  .input_box {
+    width: 100%;
+    height: 7vh;
+  }
+  .progressLoad {
+    width: 100%;
+    height: 7vh;
+  }
+  .input_box p {
+    font-size: 2vh;
+  }
+  .submitButton {
+    width:50%;
+
+  }
+  .hello {
+    width: 100%;
+  }
 }
 </style>
